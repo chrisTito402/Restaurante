1beeeb7fba8962bdca14a1c9ee97566a7cd1c15c
chrisTito402 <christopher.alvarez251954@potros.itson.edu.mx>
2025-04-01 14:15:48 -0700

    Inicio2

--- restaurantePersistencia/src/main/java/com/mycompany/restaurantepersistencia/RestaurantePersistencia.java
+++ restaurantePersistencia/src/main/java/com/mycompany/restaurantepersistencia/RestaurantePersistencia.java
@@ -11,6 +11,6 @@ package com.mycompany.restaurantepersistencia;
 public class RestaurantePersistencia {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
     }
 }
